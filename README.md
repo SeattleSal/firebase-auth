91bdb87a72f80d67504e20f775dfd152101c8fa5
Sally Perez <68660009+SeattleSal@users.noreply.github.com>
2021-09-18 10:42:36 -0700

    added .env to gitignore

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ intent is to play with firebase auth in a React project. I want to get the follo
 - Created React project using npx create-react-app
 - Created firebase project
 - Chose CSS framework - Tailwind CSS [Resource](https://tailwindcss.com/docs/guides/create-react-app)
+- Created a Sign Up page and a Login page
+- Added react-router-dom to be able to navigate different pages
+- Add firebase auth
+
+
+## TO DO
+- Make Button reusable component and prettier
--- firebase-auth-project/src/components/WelcomePage.js
+++ firebase-auth-project/src/components/WelcomePage.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 function WelcomePage() {
+
+    const handleLogout = () => {
+        alert("Log out!");
+    }
+
     return (
-        <div>
-            Welcome!
-        </div>
+        <>
+            <div className="items-center">
+                Welcome!
+            </div>
+            <button 
+                className="bg-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleLogout}>
+                Logout
+            </button>
+        </>
     )
 }
 
