be27b006de349fb23463217fb8b85d612848c67f
Sally Perez <68660009+SeattleSal@users.noreply.github.com>
2021-09-17 18:10:21 -0700

    added tailwind configs

--- firebase-auth-project/src/App.js
+++ firebase-auth-project/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={SignUpPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/welcome" component={WelcomePage}/>
+      </Switch>
+    </Router>
   );
 }
 
